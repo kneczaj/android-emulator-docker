393ebf7e22448503864cf8cdd419cda1dd266e7b
Kamil Neczaj <kneczaj@gmail.com>
2020-01-28 02:58:56 +0200

    Appium docker-compose

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f ./docker-compose-build.yaml -f ./docker-compose.yaml up --build
+docker-compose -f ./docker-compose-build.yaml -f ./android-emulator-container-scripts/js/docker/development.yaml -f ./docker-compose.yaml up --build
